9b6b2e411e4ad70427b7fb71a10478968261a440
Andrey Poleshchuck <alpolesh@gmail.com>
2020-06-05 23:50:58 +0300

    feat: add puzzle form to sentences

--- english-puzzle/src/js/index.js
+++ english-puzzle/src/js/index.js
@@ -5,21 +5,39 @@ import getWords from './API/getWords';
 import store from './store';
 
 async function addNewSentence(i) {
-  
-
   const roundSentences = await getWords(store.level, store.round);
   const currentSentence = roundSentences[i].textExample;
   
   const arr = currentSentence.split(' ');
+
+  const resultSentence = document.createElement('div');
+  resultSentence.classList.add('result-sentence');
+  document.querySelector('.puzzle-container__results').append(resultSentence);
   
-  arr.forEach(element => {
+  arr.forEach((element, index) => {
+    const maskStart = document.createElement('div');
+    maskStart.classList.add('mask');
+    const maskEnd = document.createElement('div');
+    maskEnd.classList.add('mask');
+    const wordContainer = document.createElement('div');
+    wordContainer.classList.add('word-container');
     const word = document.createElement('span');
     word.classList.add('word');
     word.innerHTML = element;
-    console.log(word)
-    document.querySelector('.puzzle-container__workspace').append(word);
+    console.log(word);
+    if (index === 0) wordContainer.style = "clip-path: polygon(calc(100% - 20px) 0, 100% 50%, calc(100% - 20px) 100%, 0 100%, 0 0);"
+    else if (index === arr.length-1) wordContainer.style = "clip-path: polygon(100% 0, 100% 100%, 0 100%, 0 100%, 20px 50%, 0 0);"
+    else wordContainer.style = "clip-path: polygon(calc(100% - 20px) 0, 100% 50%, calc(100% - 20px) 100%, 0 100%, 20px 50%, 0 0);"
+    
+    wordContainer.append(maskStart);
+    wordContainer.append(word);
+    wordContainer.append(maskEnd);
+    document.querySelector('.puzzle-container__workspace').append(wordContainer);
+    
+    wordContainer.addEventListener('click', (e) => {
+      resultSentence.append(wordContainer);
+    })
   });
-  
   
 }
 
